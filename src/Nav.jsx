3814349d1e87d45e9d1d6390fc1ae93934c32203
xerra <zbbergma@gmail.com>
2017-06-28 16:17:51 -0500

    (prog) Fix final changes for Jalso-2 starter

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,14 +7,14 @@ const Navigation = () => (
   <Navbar fluid staticTop inverse collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to='/'>Zachary Bergmann</Link>
+        <Link to='/'>Jalso-2</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav pullRight>
         <IndexLinkContainer to='/'><NavItem>Home</NavItem></IndexLinkContainer>
-        <LinkContainer to='/blog'><NavItem>Blog</NavItem></LinkContainer>
+        <LinkContainer to='/projects'><NavItem>Projects</NavItem></LinkContainer>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
